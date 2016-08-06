3b559bc753c0e09504c1e99b04696dc453d83c47
Shiyu Zhang <xanatower@users.noreply.github.com>
2016-08-07 01:23:14 +1000

    Update Part2_2.m

--- Part2_2.m
+++ Part2_2.m
@@ -1,9 +1,9 @@
-XC=0;YC=0;ZC=0;XR=1;YR=1;ZR=2;N=6;
+XC=0;YC=0;ZC=0;XR=1;YR=1;ZR=2;N=30;
 [xx,yy,zz] = ellipsoid(XC,YC,ZC,XR,YR,ZR,N);
- figure(10);
- surfl(xx,yy,zz);
- xlabel('x'); ylabel('y'); zlabel('z');
- grid on;
+figure(10);
+surfl(xx,yy,zz);
+xlabel('x'); ylabel('y'); zlabel('z');
+grid on;
 axis([-2 2 -2 2 -2 2]);%re-align the axis of x and y
 
 
